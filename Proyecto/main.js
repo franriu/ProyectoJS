65894eee23eca3c1feab9a5e0d34d3c464fdd77a
franriu <frann@hotmail.com>
2025-01-20 13:47:49 -0300

    entrega2

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -11,29 +11,39 @@ const Producto = function(nombre,precio,stock){
     this.stock = stock 
 }
 
-let producto1 =new Producto("notebook lenovo",16500,20)
-let producto2 =new Producto("notebook asus",16500,20)
-let producto3 =new Producto("notebook lg",16500,20)
-let producto4 =new Producto("notebook mac",16500,20)
-let producto5 =new Producto("notebook samsung",16500,20)
-let producto6 =new Producto("notebook razer",16500,20)
+let producto1 =new Producto("jordan4",380000)
+let producto2 =new Producto("superstar",150000)
+let producto3 =new Producto("yeezy700",360000)
+let producto4 =new Producto("airMax",200000)
+let producto5 =new Producto("newBalance9070",320000)
+let producto6 =new Producto("osiris",300000)
 
 let lista = [producto1,producto2,producto3,producto4.producto5,producto6]
+
+function filtrarProducto(){
+    let palabraClave = promp("ingresa el sneaker que buscas")
+    let resultado = lista.filter((x)=>x.nombre.toUpperCase().includes(palabraClave))
+    if (resultado.length>0){
+        console.table(resultado)
+    }else{
+        alert("intenta buscando otra sneaker" )
+    }
+}
 
 
 function agregarProducto(){
 
 let nombre=prompt("ingresa el nombre del producto")
 let precio = prompt("ingresa el precio del producto")
-let stock = prompt ("inrgresa el stock del producto")
+let stock = prompt ("ingresa el talle del producto")
 
 
-if(isNaN(precio) || isNaN(stock) || nombre ==""){
+if(isNaN(precio) || isNaN(talle) || nombre ==""){
     alert("por favor ingresa valores validos")
     return
 }
 
-let producto = new Producto (nombre,precio,stock)
+let producto = new Producto (nombre,talle,stock)
 lista.push(producto)
 console.table(lista)
 
--- clase9/main.js
+++ clase9/main.js
@@ -1,6 +1,40 @@
-let parrafo = document.getElementById("parrafo")
+/*let parrafo = document.getElementById("parrafo")
 
 console.log(parrafo.innerHTML)
 
-parrafo.innerText = "hola"
 
+parrafo.innerHTML = <button> buen dia </button>//reconoce etiquetas de html
+parrafo.innerText = "hola"    //No reconoce etiquetas dentro de html,solo texto
+
+
+let parrafo = document.getElementsByClassName ("parrafo") //llamado a la clase que designe en html 
+
+console.log(parrafo[2].innerHTML) //llamado especifico a una clase de html
+
+let letras= document.getElementsByTagNameNS("p")//llamado por el tipo de etiqueta }
+
+*/
+
+/*
+let parrafo = document.createElement("p")
+parrafo.innerHTML = "<h2> Hola coder! </h2>" //insertamos un texto desde js a html
+
+document.body.append(parrafo)
+*/
+const productos = [
+    {id:1, nombre:"Arroz", precio:125},
+    {id:2, nombre:"fideos", precio:141},
+    {id:3, nombre:"pan",precio:165},
+    {id:4,nombre:"flan",precio:75},
+
+]
+for (const x of productos){
+    let contenedor= document.createElement("div")
+    contenedor.innerHTML=`<h3>ID: ${x.id}<h3>
+                                <h1> Producto: ${x.nombre}</h1
+                                <b> Precio : ${x.precio}</b>
+                            `
+document.body.appendChild(contenedor)
+}
+
+let nombre1 = document.querySelector("") //selecciona segun clase o id 
